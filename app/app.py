0f4a408a183dd2d88a5fb4239326457e4f2165c7
sarina <sarina.sa1386.1386@gmail.com>
2023-02-28 18:28:34 +0330

    ایجاد جدول اولیه

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, url_for
+from flask_login import LoginManager, login_user, current_user, logout_user
+from models import db, Users
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 app.debug = True
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 @app.route('/')
 def home():
---
+++ app/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+
+class Users(db.Model, UserMixin):
+    uid = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(12), nullable=False, unique=True)
+    fname = db.Column(db.String(12), nullable=False)
+    lname = db.Column(db.String(12), nullable=False)
+    grade = db.Column(db.Integer, default=9)
+    school = db.Column(db.String(30))
+    score = db.Column(db.Integer, default=0)
+
+    def get_id(self):
+        return self.uid
+    
+    def __repr__(self):
+        return f'User {self.uid} - {self.user_name}'
+
+
+
+
