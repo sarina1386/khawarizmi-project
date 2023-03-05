30dc959d002c7b4f72024f0d5140a24eb73c9cd1
sarina <sarina.sa1386.1386@gmail.com>
2023-03-05 17:47:30 +0330

    ساخت صفحه ثبت نام

--- app/app.py
+++ app/app.py
@@ -6,22 +6,33 @@ from flask_migrate import Migrate
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 app.debug = True
+app.config['SECRET_KEY'] = 'sarina_sonia'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db.init_app(app)
 migrate = Migrate(app, db)
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view = 'login'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
 
 
 
