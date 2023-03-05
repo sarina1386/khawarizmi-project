d5ba6ade07a0c90e04841effc1b737f247c7c225
sarina <sarina.sa1386.1386@gmail.com>
2023-03-05 17:30:51 +0330

    ساخت دیتابیس

--- app/app.py
+++ app/app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, url_for
 from flask_login import LoginManager, login_user, current_user, logout_user
 from models import db, Users
+from flask_migrate import Migrate
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 app.debug = True
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db.init_app(app)
+migrate = Migrate(app, db)
+
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'login'
 
 @app.route('/')
 def home():
